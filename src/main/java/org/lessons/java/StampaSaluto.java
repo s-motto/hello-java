413a87c1575cd17a737c085c28bd4a4b66ae71e1
s-motto <brinamotto@gmail.com>
2024-06-03 22:17:14 +0200

    added StampaSaluto

---
+++ src/main/java/org/lessons/java/StampaSaluto.java
@@ -0,0 +1,10 @@
+package org.lessons.java;
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.printf("Hello Java");
+	}
+
+}
